fd1f97b89275ae5c65246878982a68802670853f
akeenkarkare <61316992+akeenkarkare@users.noreply.github.com>
2021-02-21 17:30:41 +0530

    Add files via upload
    You can create a new file for the code to read, but don't forget to call it in the python code! More instructions given in the python file.

---
+++ Document-reader.py
@@ -0,0 +1,6 @@
+# This line is calling the variable and calling the open function to open a specific file given in the string
+# Insert your file name in the string, in this case, this is the second file which I uploaded in my repository
+time_table_fd = open('test-read.txt')
+
+# This prints the what the variable was told to do
+print(time_table_fd)
